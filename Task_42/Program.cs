ddb21fe75753e2687314066d5d240ed1102d3f83
c8ler <kotovsergey@yandex.ru>
2022-06-05 11:52:29 +0300

    5-6-2022

---
+++ Task_42/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в разную систему счисления.
+
+bool run = true;
+while (run)
+{
+    Console.Clear();
+    Console.Write("Введите число: ");
+    int number = int.Parse(Console.ReadLine());
+    Console.Write("Введите систему счисления: ");
+    int baseNumber = int.Parse(Console.ReadLine());
+
+    DecToOther(number, baseNumber);
+    Console.WriteLine();
+
+    Console.WriteLine("Попробуем ещё раз? (y/n): ");
+    run = Console.ReadKey().Key == ConsoleKey.Y;
+}
+
+void DecToOther(int num, int baseN)
+{
+    if (num == 0) return;
+    DecToOther(num / baseN, baseN);
+    Console.Write(num % baseN);
+}
---
+++ Task_46/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Задача 46: Задайте двумерный массив размером m×n,
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1 */
+
+Console.Clear();
+int[,] GetArray(int rows, int cols)
+{
+    int[,] array = new int[rows, cols];
+    Random newRnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = newRnd.Next(10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] arr = GetArray(3, 4);
+PrintArray(arr);
