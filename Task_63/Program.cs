db55622b364fdaac5b2ef9163ff7745cc3b7b8fa
c8ler <kotovsergey@yandex.ru>
2022-06-12 00:44:37 +0300

    12-06-2022

---
+++ Task_63/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Задача 63: Задайте значение N. Напишите программу, которая выведет
+все натуральные числа в промежутке от 1 до N.
+N = 5 -> "1, 2, 3, 4, 5"
+N  = 6 -> "1, 2, 3, 4, 5, 6" */
+
+Console.Clear();
+Console.Write("Задайте число: ");
+int n = int.Parse(Console.ReadLine());
+NaturalNumber(n);
+
+void NaturalNumber(int num)
+{
+    if (num == 0) return;
+    NaturalNumber(num - 1);
+    Console.Write($"{num} ");
+}
---
+++ Task_65/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 65: Задайте значения M и N. Напишите программу, которая
+выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5 -> "1, 2, 3, 4, 5"
+M = 4; N = 8 -> "4, 6, 7, 8" */
+
+Console.Clear();
+Console.Write("Задайте начальное число M: ");
+int mm = int.Parse(Console.ReadLine());
+Console.Write("Задайте конечное число N: ");
+int nn = int.Parse(Console.ReadLine());
+
+if (mm < nn) NaturalNumber(mm, nn);
+else NaturalNumber(nn, mm);
+
+void NaturalNumber(int m, int n)
+{
+    if (m > n) return;
+    NaturalNumber(m, n - 1);
+    Console.Write($"{n} ");
+}
